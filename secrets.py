42aa7591f6569f19047cc7d709bc55777375af40
onceuponapril <31635476+onceuponapril@users.noreply.github.com>
2018-04-20 22:28:26 -0400

    Delete secrets.py

--- secrets.py
+++
@@ -1,6 +0,0 @@
-map_api='AIzaSyDkbSY6il_X6f4gCCUP8Yj9OeLEYNg60R4'
-client_id='KZ-TZqsDDPf9'
-client_secret='K7GU9SoiwvUMcr-QyzQ_ynaqlGpEF9Ww'
-access_token='7ze30S6B+qvt4f7vet2nm6PHg8q8wfQWEIg3lxeP1EPgHoLFagPZuevnqm07lXx19gwXZZMNdhwSRuCc2yVG+NacsXJifU3M8sJyjMavSvz1t6RpLCmL8u4='
-scope= "public"
-token_type='bearer'
